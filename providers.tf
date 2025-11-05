9cdfaaf3c295a3b0c40e7ddc076ef907b2a5cb8d
Debadatta Gadanayak <dkgadanayak143@gmail.com>
2025-11-05 06:09:11 +0000

    backend configured

--- providers.tf
+++ providers.tf
@@ -10,5 +10,14 @@ terraform {
 provider "aws" {
   region = "ap-southeast-2"
 }
-
-
+ 
+terraform {
+  backend "s3" {
+    bucket = "dev-roche-terraform-bucket"
+    key    = "dev/deb/terraform.tfstate"
+    encrypt = true
+    dynamodb_table = "dev-roche-table1"
+    region = "ap-southeast-2"
+    
+  }
+}
