92532697d46906df7c8b4fd5cc87cb343c514732
Debadatta Gadanayak <dkgadanayak143@gmail.com>
2025-11-07 07:29:39 +0000

    fixed SG

--- modules/ec2/aws-security-group-create.tf
+++ modules/ec2/aws-security-group-create.tf
@@ -17,7 +17,7 @@ resource "aws_security_group" "allow_tls" {
 # Ingress Rules
 resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
   security_group_id = aws_security_group.allow_tls.id
-  cidr_ipv4         = data.aws_vpc.example.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 80
   ip_protocol       = "tcp"
   to_port           = 80
